c3c8d0781dbccc559ad317af7d27506534a8b50f
Umut Polat <umut.polat@campus.tu-berlin.de>
2025-12-16 12:05:26 +0100

    Replace LinearMaps import with ProgressMeter

--- src/Resolvent/utils.jl
+++ src/Resolvent/utils.jl
@@ -1,6 +1,6 @@
 module Utils
 
-import LinearAlgebra, IterativeSolvers, LinearMaps
+import LinearAlgebra, IterativeSolvers, ProgressMeter
 include("Resolvent_analysis_exports.jl")
 include("NLEVP.jl")
 include("Helmholtz.jl")
