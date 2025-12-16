6b7000c189a4f7d3f31e19c350e82842d610959c
Umut Polat <umut.polat@campus.tu-berlin.de>
2025-12-16 12:04:34 +0100

    Consolidate imports and add include statements
    Refactor imports and include statements in utils.jl

--- src/Resolvent/utils.jl
+++ src/Resolvent/utils.jl
@@ -1,7 +1,8 @@
 module Utils
 
-using LinearAlgebra
-using IterativeSolvers
-using LinearMaps
+import LinearAlgebra, IterativeSolvers, LinearMaps
+include("Resolvent_analysis_exports.jl")
+include("NLEVP.jl")
+include("Helmholtz.jl")
 
 end
