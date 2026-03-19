da13a70f100618916d84b396c6492e7a667a17a0
Umut Polat <umut.polat@campus.tu-berlin.de>
2026-03-19 16:18:24 +0100

    Add Statistics and Optim imports to Utils module

--- src/Resolvent/utils.jl
+++ src/Resolvent/utils.jl
@@ -1,6 +1,6 @@
 module Utils
 
-import LinearAlgebra, SparseArrays, ProgressMeter, Plots
+import LinearAlgebra, SparseArrays, ProgressMeter, Plots, Statistics, Optim
 include("Resolvent_analysis_exports.jl")
 include("NLEVP.jl")
 include("Helmholtz.jl")
